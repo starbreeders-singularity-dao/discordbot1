e71d3e060e8f15c1ccfe6f9aed7bd8dac37c815f
Jonathan Shyaka <jonashyaka2@gmail.com>
2024-05-31 13:51:32 +0200

    Handle discord client stop

--- discordBot.js
+++ discordBot.js
@@ -12,27 +12,45 @@ dotenv.config();
 
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 
-console.log('Starting Discord bot...');
-
-if (!DISCORD_TOKEN) {
-    console.error('DISCORD_TOKEN is not set');
-    process.exit(1);
-}
-
-client.once('ready', () => {
-    console.log('Discord bot is ready and connected!');
-});
-
-client.on('messageCreate', message => {
-    console.log(`Received message: ${message.content}`);
-    if (message.content === '!invite') {
-        message.channel.send('Your invite code is: YOUR_INVITE_CODE');
-        console.log('Sent invite code');
-    }
-});
+let isBotRunning = false;
 
 module.exports = {
     start: () => {
-        client.login(DISCORD_TOKEN).catch(err => console.error('Failed to login:', err));
+        if (!DISCORD_TOKEN) {
+            console.error('DISCORD_TOKEN is not set');
+            process.exit(1);
+        }
+
+        client.login(DISCORD_TOKEN)
+            .then(() => {isBotRunning = true;})
+            .catch(err => console.error('Failed to login:', err));
+
+        client.once('ready', () => {
+            console.log('Discord bot is ready and connected!');
+        });
+        
+        client.on('messageCreate', message => {
+            console.log(`Received message: ${message.content}`);
+            if (message.content === '!invite') {
+                message.channel.send('Your invite code is: YOUR_INVITE_CODE');
+                console.log('Sent invite code');
+            }
+        });
+    },
+    stop: () => {
+        if (!isBotRunning) {
+            console.log('Discord bot is not running.');
+            if (callback) callback();
+            return;
+        }
+
+        client.destroy().then(() => {
+            console.log('Discord bot stopped.');
+            isBotRunning = false;
+            if (callback) callback();
+        }).catch(err => {
+            console.error('Failed to stop Discord bot:', err);
+            if (callback) callback(err);
+        });
     }
 };
--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const http = require('http');
 const discordBot = require('./discordBot');
 
-console.log('Starting Discord bot...');
+function startServer() {
+    const server = http.createServer(function (req, res) {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('OK');
+    }).listen(8080);
+
+    console.log('Server running');
+    return server;
+}
+
 discordBot.start();
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('OK');
-}).listen(8080);
+startServer();
+
+function handleExit(signal) {
+    console.log(`Received ${signal}. Stopping discordBot...`);
+    discordBot.stop(() => {
+        console.log('discordBot stopped.');
+        process.exit(0);
+    });
+}
+
+process.on('SIGINT', handleExit);
+process.on('SIGTERM', handleExit);
+process.on('exit', handleExit);
